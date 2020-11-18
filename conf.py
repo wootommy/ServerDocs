ea0d36a272bd72819c4bc20ae888d076571344ec
wootommy <woo.tommy@icloud.com>
2020-11-18 15:50:43 +0800

    Add AutoStructify Component.
    Update:
    - Now we are ready ro use more fancy styles in .md files.
    - Maintained conf.py.

--- conf.py
+++ conf.py
@@ -1,25 +1,71 @@
-# -- Path setup --------------------------------------------------------------
+"""
+FilePath: /conf.py
+LastEditTime: 2020-11-14 20:50
+Author: tommy
+Description: Config file for sphinx project.
+"""
+# global ref:
+# https://github.com/readthedocs/recommonmark/blob/master/docs/conf.py
+
+# -------------------------------- Path setup -------------------------------- #
 
 import sphinx_rtd_theme
+import recommonmark
+from recommonmark.transform import AutoStructify
 
-
-# -- Project information -----------------------------------------------------
+# ---------------------------- Project information --------------------------- #
 
 project = "NaMI Server Docs"
 copyright = "2020, nami-442"
-author = "nami-442"
+author = "nami-442@tommy"
 
+# --------------------------- General configuration -------------------------- #
 
-# -- General configuration ---------------------------------------------------
-
-extensions = ["sphinx_rtd_theme", "recommonmark"]
-templates_path = ["_templates"]
-exclude_patterns = []
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
+    "recommonmark",
+]
 master_doc = "index"
+exclude_patterns = ["_build"]
 
-
-# -- Options for HTML output -------------------------------------------------
+# -------------------------- Options for HTML output ------------------------- #
+# ref: https://sphinx-rtd-theme.readthedocs.io/en/latest/configuring.html#
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_theme_options = {
+    # toc options
+    "navigation_depth": 3,
+    "includehidden": True,
+    "titles_only": False,
+    # display options
+    "display_version": False,
+    "style_external_links": True,
+}
+html_logo = None
+html_favicon = None
+html_last_updated_fmt = "%b %d, %Y"
+html_show_sphinx = False
+html_baseurl = "./files"
 
+# ------------------------- Options for AutoStructify ------------------------ #
+
+# At the bottom of conf.py
+# github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
+def setup(app):
+    app.add_config_value(
+        "recommonmark_config",
+        {
+            "enable_auto_toc_tree": True,
+            "auto_toc_tree_section": "Contents",
+            # "enable_auto_doc_ref": True,
+            "enable_math": True,
+            "enable_inline_math": True,
+            "enable_eval_rst": True,
+            # "url_resolver": lambda url: github_doc_root + url,
+        },
+        True,
+    )
+    app.add_transform(AutoStructify)
--- files/05/contact.md
+++ files/05/contact.md
@@ -2,5 +2,47 @@
 
 如果有任何问题，请咨询常年不在线的管理员。
 
-- **Location**：同济大学嘉定校区，济事楼308
-- **Number**：188\*\*\*\*\*\*09
+-   **Location**：同济大学嘉定校区，济事楼 308
+-   **Number**：188\*\*\*\*\*\*09
+
+```important:: Its a note! in markdown!
+
+```
+
+```warning:: Its a note! in markdown!
+
+```
+
+```note:: Its a note! in markdown!
+
+```
+
+```eval_rst
+.. autoclass:: recommonmark.transform.AutoStructify
+    :show-inheritance:
+```
+
+```sidebar:: Line numbers and highlights
+
+     emphasis-lines:
+       highlights the lines.
+     linenos:
+       shows the line numbers as well.
+     caption:
+       shown at the top of the code block.
+     name:
+       may be referenced with `:ref:` later.
+```
+
+```code-block::
+     :linenos:
+     :emphasize-lines: 3,5
+     :caption: An example code-block with everything turned on.
+     :name: Full code-block example
+
+     # Comment line
+     import System
+     System.run_emphasis_line
+     # Long lines in code blocks create a auto horizontal scrollbar
+     System.exit!
+```
